ea3503ccd2568d6abfd6eb7288f47fa67fd80b44
Josema <enzo@josema.es>
2026-06-25 10:04:26 +0200

    Added clipping

--- src/renderer/RendererWebGPU.ts
+++ src/renderer/RendererWebGPU.ts
@@ -1,5 +1,6 @@
 import Renderer from '../Renderer.ts'
 import createEngine, { YOGA_SETTER } from '../engine/yoga.ts'
+import { getAncestorClipping } from '../utils/getAncestorClipping.ts'
 
 export default class RendererWebGPU extends Renderer {
     // private node_states = new WeakMap()
@@ -45,7 +46,7 @@ export default class RendererWebGPU extends Renderer {
             layout: 'auto',
             vertex: {
                 module: this.device.createShaderModule({
-                    code: rectangleVertWGSL,
+                    code: nodeVertWGSL,
                 }),
                 entryPoint: 'main',
                 buffers: [
@@ -60,19 +61,24 @@ export default class RendererWebGPU extends Renderer {
                         ],
                     },
                     {
-                        arrayStride: INSTANCE_SIZE,
+                        arrayStride: ATTRIBUTES_SIZE,
                         stepMode: 'instance',
                         /* pretty-ignore */
                         attributes: [
                             {
-                                shaderLocation: BUFFER_FIELDS.LAYOUT.shader_location,
-                                offset: BUFFER_FIELDS.LAYOUT.byte_offset,
-                                format: BUFFER_FIELDS.LAYOUT.format,
+                                shaderLocation: ATTRIBUTES.LAYOUT.LOCATION,
+                                offset: ATTRIBUTES.LAYOUT.OFFSET,
+                                format: ATTRIBUTES.LAYOUT.FORMAT,
                             },
                             {
-                                shaderLocation: BUFFER_FIELDS.BACKGROUNDCOLOR.shader_location,
-                                offset: BUFFER_FIELDS.BACKGROUNDCOLOR.byte_offset,
-                                format: BUFFER_FIELDS.BACKGROUNDCOLOR.format,
+                                shaderLocation: ATTRIBUTES.CLIPPING.LOCATION,
+                                offset: ATTRIBUTES.CLIPPING.OFFSET,
+                                format: ATTRIBUTES.CLIPPING.FORMAT,
+                            },
+                            {
+                                shaderLocation: ATTRIBUTES.BACKGROUNDCOLOR.LOCATION,
+                                offset: ATTRIBUTES.BACKGROUNDCOLOR.OFFSET,
+                                format: ATTRIBUTES.BACKGROUNDCOLOR.FORMAT,
                             },
                         ],
                     },
@@ -80,7 +86,7 @@ export default class RendererWebGPU extends Renderer {
             },
             fragment: {
                 module: this.device.createShaderModule({
-                    code: rectangleFragWGSL,
+                    code: nodeFragWGSL,
                 }),
                 entryPoint: 'main',
                 targets: [
@@ -162,7 +168,7 @@ export default class RendererWebGPU extends Renderer {
 
     private draw(nodes) {
         const node_instances = this.createInstancesNodes(nodes)
-        const node_instances_count = node_instances.bytes_offset / INSTANCE_SIZE
+        const node_instances_count = node_instances.bytes_offset / ATTRIBUTES_SIZE
         const command_encoder = this.device.createCommandEncoder()
         const texture_view = this.context.getCurrentTexture().createView()
         const pass_encoder = command_encoder.beginRenderPass({
@@ -180,7 +186,7 @@ export default class RendererWebGPU extends Renderer {
         if (node_instances_count > 0) {
             // If the buffer is too small to hold all the node instances
             // we destroy the old buffer and create a new one with the required size.
-            if (this.nodes_buffer_size < node_instances.bytes_offset) {
+            if (this.nodes_buffer_size < node_instances.bytes_offset || !this.nodes_buffer) {
                 this.nodes_buffer_size = node_instances.bytes_offset
                 this.nodes_buffer?.destroy()
                 this.nodes_buffer = this.device.createBuffer({
@@ -214,19 +220,20 @@ export default class RendererWebGPU extends Renderer {
 
     // This function creates a buffer containing all the data prepared for the GPU to render the nodes.
     private createInstancesNodes(nodes) {
-        const buffer = new ArrayBuffer(nodes.length * INSTANCE_SIZE)
+        const buffer = new ArrayBuffer(nodes.length * ATTRIBUTES_SIZE)
         const floats = new Float32Array(buffer)
         const bytes = new Uint8Array(buffer)
         let bytes_offset = 0
 
         for (const node of nodes) {
-            if (!isNodeDrawable(node)) {
+            const clipping = getAncestorClipping(node)
+
+            if (!isNodeDrawable(node, clipping)) {
                 continue
             }
 
             // Layout: x, y, width, height
-            const layout_float_offset =
-                (bytes_offset + BUFFER_FIELDS.LAYOUT.byte_offset) / FLOAT32_SIZE
+            const layout_float_offset = (bytes_offset + ATTRIBUTES.LAYOUT.OFFSET) / FLOAT32_SIZE
             const { x, y, width, height } = node.layout
             floats[layout_float_offset + 0] = x
             floats[layout_float_offset + 1] = y
@@ -234,28 +241,42 @@ export default class RendererWebGPU extends Renderer {
             floats[layout_float_offset + 3] = height
 
             // backgroundColor: r, g, b, a
-            const bgcolor_bytes_offset = bytes_offset + BUFFER_FIELDS.BACKGROUNDCOLOR.byte_offset
+            const bgcolor_bytes_offset = bytes_offset + ATTRIBUTES.BACKGROUNDCOLOR.OFFSET
             const [r, g, b, a] = node.styles.backgroundColor.parsed.rgba
             bytes[bgcolor_bytes_offset + 0] = r
             bytes[bgcolor_bytes_offset + 1] = g
             bytes[bgcolor_bytes_offset + 2] = b
             bytes[bgcolor_bytes_offset + 3] = a
 
-            bytes_offset += INSTANCE_SIZE
+            if (clipping !== null) {
+                const clipping_float_offset =
+                    (bytes_offset + ATTRIBUTES.CLIPPING.OFFSET) / FLOAT32_SIZE
+                floats[clipping_float_offset + 0] = clipping.top
+                floats[clipping_float_offset + 1] = clipping.right
+                floats[clipping_float_offset + 2] = clipping.bottom
+                floats[clipping_float_offset + 3] = clipping.left
+            }
+
+            bytes_offset += ATTRIBUTES_SIZE
         }
 
         return { bytes, bytes_offset }
     }
 }
 
-function isNodeDrawable(node) {
+function isNodeDrawable(node, clip) {
+    const { width, height } = node.layout
+    const { backgroundColor } = node.styles
+
     return (
-        node.layout.width > 0 &&
-        node.layout.height > 0 &&
-        (node.styles.backgroundColor !== undefined || node.styles.backgroundColor.rgba[3] > 0)
-        // node.styles.display !== 'none' &&
-        // node.styles.visibility !== 'hidden' &&
-        // node.styles.opacity !== 0 &&
+        width > 0 &&
+        height > 0 &&
+        (clip === null || (clip.left + clip.right < width && clip.top + clip.bottom < height)) &&
+        backgroundColor !== undefined &&
+        backgroundColor.parsed.rgba[3] > 0
+        // display !== 'none' &&
+        // visibility !== 'hidden' &&
+        // opacity !== 0 &&
     )
 }
 
@@ -265,27 +286,31 @@ const POSITION_VERTEX_COUNT = 6
 const POSITION_VERTEX_FLOATS = 2
 const POSITION_VERTEX_SIZE = POSITION_VERTEX_FLOATS * FLOAT32_SIZE
 const POSITION_VERTICES = new Float32Array([0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1])
-const BUFFER_FIELDS = {
+const ATTRIBUTES = {
     LAYOUT: {
-        shader_location: 1,
-        byte_offset: 0,
-        byte_size: 4 * FLOAT32_SIZE,
-        format: 'float32x4',
+        LOCATION: 1,
+        OFFSET: 0,
+        SIZE: 4 * FLOAT32_SIZE,
+        FORMAT: 'float32x4',
+    },
+    CLIPPING: {
+        LOCATION: 2,
+        OFFSET: 4 * FLOAT32_SIZE + 4,
+        SIZE: 4 * FLOAT32_SIZE,
+        FORMAT: 'float32x4',
     },
     BACKGROUNDCOLOR: {
-        shader_location: 2,
-        byte_offset: 4 * FLOAT32_SIZE,
-        byte_size: 4,
-        format: 'unorm8x4',
+        LOCATION: 3,
+        OFFSET: 4 * FLOAT32_SIZE,
+        SIZE: 4,
+        FORMAT: 'unorm8x4',
     },
 }
-const INSTANCE_SIZE = Math.max(
-    ...Object.values(BUFFER_FIELDS).map(
-        (instance_field) => instance_field.byte_offset + instance_field.byte_size,
-    ),
+const ATTRIBUTES_SIZE = Math.max(
+    ...Object.values(ATTRIBUTES).map((attrb) => attrb.OFFSET + attrb.SIZE),
 )
 
-const rectangleVertWGSL = /* wgsl */ `
+const nodeVertWGSL = /* wgsl */ `
 struct Viewport {
   size: vec2f,
   padding: vec2f,
@@ -293,7 +318,10 @@ struct Viewport {
 
 struct VertexOutput {
   @builtin(position) position: vec4f,
-  @location(0) background_color: vec4f,
+  @location(0) local_position: vec2f,
+  @location(1) rect_size: vec2f,
+  @location(2) clipping: vec4f,
+  @location(3) background_color: vec4f,
 }
 
 @group(0) @binding(0) var<uniform> viewport: Viewport;
@@ -302,7 +330,8 @@ struct VertexOutput {
 fn main(
   @location(0) position: vec2f,
   @location(1) layout_node: vec4f,
-  @location(2) background_color: vec4f,
+  @location(2) clipping: vec4f,
+  @location(3) background_color: vec4f,
 ) -> VertexOutput {
   let local_position = position * layout_node.zw;
   let pixel = layout_node.xy + local_position;
@@ -313,18 +342,33 @@ fn main(
 
   var output: VertexOutput;
   output.position = vec4f(clip, 0.0, 1.0);
+  output.local_position = local_position;
+  output.rect_size = layout_node.zw;
   output.background_color = background_color;
+  output.clipping = clipping;
   return output;
 }
 `
 
-const rectangleFragWGSL = /* wgsl */ `
+const nodeFragWGSL = /* wgsl */ `
 struct FragmentInput {
-  @location(0) background_color: vec4f,
+  @location(0) local_position: vec2f,
+  @location(1) rect_size: vec2f,
+  @location(2) clipping: vec4f,
+  @location(3) background_color: vec4f,
 }
 
 @fragment
 fn main(input: FragmentInput) -> @location(0) vec4f {
+  if (
+    input.local_position.x < input.clipping.w ||
+    input.local_position.y < input.clipping.x ||
+    input.local_position.x > input.rect_size.x - input.clipping.y ||
+    input.local_position.y > input.rect_size.y - input.clipping.z
+  ) {
+    discard;
+  }
+
   return input.background_color;
 }
 `
