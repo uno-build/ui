c1bd02d9255a97212c9f8eb9c6e1d02cdb8e51d9
Josema <enzo@josema.es>
2026-07-30 18:39:49 +0200

    Improving examples

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,12 +5,12 @@ export default defineConfig({
     outputDir: './tests/.results',
     webServer: {
         command: 'npm run dev -- --host 127.0.0.1',
-        url: 'http://127.0.0.1:5173',
+        url: 'http://127.0.0.1:5173/dev/',
         reuseExistingServer: !process.env.CI,
     },
     use: {
         ...devices['Desktop Chrome'],
-        baseURL: 'http://127.0.0.1:5173',
+        baseURL: 'http://127.0.0.1:5173/dev/',
         viewport: { width: 800, height: 600 },
         launchOptions: {
             args: ['--enable-unsafe-webgpu'],
