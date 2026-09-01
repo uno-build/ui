0914d69f40210882e2196e252e9df34d50f66fa0
Josema <enzo@josema.es>
2026-09-01 16:42:51 +0200

    Organizing octane examples

--- examples/vite/octane.config.ts
+++ examples/vite/octane.config.ts
@@ -1,19 +1,15 @@
-import { readdirSync } from 'node:fs'
 import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import { octane } from '@octanejs/vite-plugin'
 import { rendererConfig } from '../../src/components/octane/config.js'
 
-const COMPONENT_EXAMPLES_URL = new URL('../components/', import.meta.url)
-const OCTANE_EXAMPLE_INPUTS = readdirSync(COMPONENT_EXAMPLES_URL)
-    .filter((file_name) => file_name.startsWith('octane-') && file_name.endsWith('.html'))
-    .map((file_name) => fileURLToPath(new URL(file_name, COMPONENT_EXAMPLES_URL)))
+const OCTANE_EXAMPLE_INPUT = fileURLToPath(new URL('../components/octane.html', import.meta.url))
 
 export default defineConfig({
     plugins: [octane(rendererConfig)],
     build: {
         rollupOptions: {
-            input: OCTANE_EXAMPLE_INPUTS,
+            input: OCTANE_EXAMPLE_INPUT,
         },
     },
 })
