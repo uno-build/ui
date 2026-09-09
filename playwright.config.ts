3577003d5b91584a3040dd34bf5eb99845511c6d
Josema <enzo@josema.es>
2026-09-09 14:24:15 +0200

    Fix: scripts

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     webServer: {
         command: 'vite ./dev/layouts --host 127.0.0.1',
         url: 'http://127.0.0.1:5173/dev/layouts/',
-        reuseExistingServer: !process.env.CI,
+        reuseExistingServer: false,
     },
     use: {
         ...devices['Desktop Chrome'],
